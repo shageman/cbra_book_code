5ed254cdb1b58479be560dd4641551afad5bf577
Stephan Hagemann <stephan.hagemann@gmail.com>
2016-07-03 09:52:45 +0200

    C5S02: fixed prediction specs

--- c5s02/sportsball/components/predictor/spec/predictor_spec.rb
+++ c5s02/sportsball/components/predictor/spec/predictor_spec.rb
@@ -1,20 +1,27 @@
-RSpec.describe Predictor::Predictor do
+RSpec.describe "Prediction process" do
   before do
     @team1 = OpenStruct.new(id: 6)
     @team2 = OpenStruct.new(id: 7)
 
-    @predictor = Predictor::Predictor.new([@team1, @team2])
+    @team1.extend Predictor::Role::Contender
+    @team1.extend Predictor::Role::Predictor
+
+    @team2.extend Predictor::Role::Contender
+
+    @team1.contenders = [@team1, @team2]
+    @team1.opponent = @team2
+
+    @predictor = @team1
   end
 
   it "predicts teams that have won in the past to win in the future" do
     game = OpenStruct.new(
         first_team_id: @team1.id, second_team_id: @team2.id, winning_team: 1)
+    game.extend Predictor::Role::HistoricalPerformanceIndicator
+
     @predictor.learn([game])
 
-    prediction = @predictor.predict(@team2, @team1)
-    expect(prediction.winner).to eq @team1
-
-    prediction = @predictor.predict(@team1, @team2)
+    prediction = @predictor.predict
     expect(prediction.winner).to eq @team1
   end
 
@@ -25,21 +32,12 @@ RSpec.describe Predictor::Predictor do
         first_team_id: @team1.id, second_team_id: @team2.id, winning_team: 2)
     game3 = OpenStruct.new(
         first_team_id: @team1.id, second_team_id: @team2.id, winning_team: 2)
+    [game1, game2, game3].each do |game|
+      game.extend Predictor::Role::HistoricalPerformanceIndicator
+    end
     @predictor.learn([game1, game2, game3])
 
-    prediction = @predictor.predict(@team1, @team2)
+    prediction = @predictor.predict
     expect(prediction.winner).to eq @team2
-  end
-
-  it "behaves funny when teams are equally strong" do
-    prediction = @predictor.predict(@team1, @team2)
-    expect(prediction.first_team).to eq @team1
-    expect(prediction.second_team).to eq @team2
-    expect(prediction.winner).to eq @team2
-
-    prediction = @predictor.predict(@team2, @team1)
-    expect(prediction.first_team).to eq @team2
-    expect(prediction.second_team).to eq @team1
-    expect(prediction.winner).to eq @team1
   end
 end
